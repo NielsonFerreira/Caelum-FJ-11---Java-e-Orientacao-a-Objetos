ac29f7432f31f0272934b8f5cc3d9935cdb84776
Nielson Ferreira <nielsonfferreira@hotmail.com>
2021-02-13 12:07:33 -0300

    Cap. 11 - INTERFACES - 11.5

--- fj11-contas/src/br/com/caelum/contas/modelo/ContaCorrente.java
+++ fj11-contas/src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -1,6 +1,6 @@
 package br.com.caelum.contas.modelo;
 
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta implements Tributavel{
 	
 	@Override
 	public void saca(double valor) {
@@ -9,5 +9,11 @@ public class ContaCorrente extends Conta{
 	
 	public String getTipo() {
 		return "Conta Corrente";
+	}
+
+	@Override
+	public double getValorImposto() {
+		
+		return this.getSaldo() * 0.01;
 	}
 }
