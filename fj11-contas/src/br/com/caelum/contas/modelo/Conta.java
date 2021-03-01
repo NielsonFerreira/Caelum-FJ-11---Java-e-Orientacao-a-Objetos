22f201ac56c51bec1acdc7848acd16e551295f31
Nielson Ferreira <nielsonfferreira@hotmail.com>
2021-03-01 11:51:28 -0300

    Cap. 13 - O PACOTE JAVA.LANG - 13.7

--- fj11-contas/src/br/com/caelum/contas/modelo/Conta.java
+++ fj11-contas/src/br/com/caelum/contas/modelo/Conta.java
@@ -93,7 +93,7 @@ public abstract class Conta {
 	
 	@Override
 	public String toString() {
-		return "[titular=" + titular + ", numero=" + numero + ", agencia=" + agencia + "]";
+		return "[titular=" + titular.toUpperCase() + ", número=" + this.numero + ", agencia=" + this.agencia + "]";
 	}
 
 	@Override
