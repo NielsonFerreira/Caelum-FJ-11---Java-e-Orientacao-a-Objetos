7eed7ceb3a25464f44016f17ec6fca03d0ff888b
Nielson Ferreira <nielsonfferreira@hotmail.com>
2021-03-03 13:18:09 -0300

    Cap. 14 - UM POUCO DE ARRAYS - 14.5

--- fj11-contas/src/br/com/caelum/contas/modelo/Conta.java
+++ fj11-contas/src/br/com/caelum/contas/modelo/Conta.java
@@ -93,7 +93,7 @@ public abstract class Conta {
 	
 	@Override
 	public String toString() {
-		return "[titular=" + titular.toUpperCase() + ", número=" + this.numero + ", agencia=" + this.agencia + "]";
+		return "[titular=" + titular.toUpperCase() + ", numero=" + this.numero + ", agencia=" + this.agencia + "]";
 	}
 
 	@Override
