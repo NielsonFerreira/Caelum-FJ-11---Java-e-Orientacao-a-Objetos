054ff1c85deeba17e5266e7e576b4c018e1f9608
Nielson Ferreira <nielsonfferreira@hotmail.com>
2021-04-03 12:30:46 -0300

    Cap. 16 - PACOTE JAVA.IO - 16.11

--- fj11-contas/src/br/com/caelum/contas/ManipuladorDeContas.java
+++ fj11-contas/src/br/com/caelum/contas/ManipuladorDeContas.java
@@ -48,4 +48,10 @@ public class ManipuladorDeContas {
 		List<Conta> contas = evento.getLista("destino");
 		Collections.sort(contas);
 	}
+	
+	public void salvaDados(Evento evento) {
+		List<Conta> contas = evento.getLista("listaContas");
+		RepositorioDeContas repositorio = new RepositorioDeContas();
+		repositorio.salva(contas);
+	}
 }
